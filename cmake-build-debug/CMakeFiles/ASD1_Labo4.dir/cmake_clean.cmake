e71f7eb0d5ce7c49192a608d7d36afff9e76abaf
Smithja <james.smith@heig-vd.ch>
2017-04-27 10:36:13 +0200

    New project

---
+++ cmake-build-debug/CMakeFiles/ASD1_Labo4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ASD1_Labo4.dir/main.cpp.obj"
+  "ASD1_Labo4.pdb"
+  "ASD1_Labo4.exe"
+  "ASD1_Labo4.exe.manifest"
+  "libASD1_Labo4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ASD1_Labo4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
