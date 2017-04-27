cb634b38c07d9b2a9e8c6b2d93c592dc705c60c7
Smithja <james.smith@heig-vd.ch>
2017-04-27 13:45:20 +0200

    test

---
+++ ASD1_Labo04_CC1/cmake-build-debug/CMakeFiles/ASD1_Labo04_CC1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ASD1_Labo04_CC1.dir/nbproject/private/c_standard_headers_indexer.c.obj"
+  "CMakeFiles/ASD1_Labo04_CC1.dir/nbproject/private/cpp_standard_headers_indexer.cpp.obj"
+  "CMakeFiles/ASD1_Labo04_CC1.dir/liste.cpp.obj"
+  "CMakeFiles/ASD1_Labo04_CC1.dir/main.cpp.obj"
+  "ASD1_Labo04_CC1.pdb"
+  "ASD1_Labo04_CC1.exe"
+  "ASD1_Labo04_CC1.exe.manifest"
+  "libASD1_Labo04_CC1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/ASD1_Labo04_CC1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
